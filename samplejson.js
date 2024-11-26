251e7c2e3e08a0e761ef9224c6af216e5d308968
Gasper <gasper.pistotnik@gmail.com>
2024-11-26 21:55:54 +0100

    Initial commit scraper

---
+++ samplejson.js
@@ -0,0 +1,251 @@
+const test = {
+    title: 'Bentley Continental GT 6.0 First Edition - Bencin - Samodejni - 635 hp - 68.845 km',
+    url: 'https: //www.openlane.eu/sl/car/info?auctionId=8755798',
+    firstReg: '10/04/2019',
+    mileage: '68.845 km',
+    fuel: 'benzín',
+    transmission: 'Samodejni menjalnik',
+    kw: '467 kW (635 Km)',
+    engineSize: '5.950 cc',
+    vin: 'SCBCD13S6KC073740',
+    color: 'Siva',
+    ddv: 'DDV je vključen',
+    lokacija: 'NL Raamsdonksveer (Nizozemska)',
+    possiblePrice: '148.255,90 €',
+    deliveryPrice: '€ 518',
+    deliveryTime: '26/12/2024 in 06/01/2025*',
+    imageUrls: [
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/45bca3c5-4081-4117-b985-6835ca654375.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/45bca3c5-4081-4117-b985-6835ca654375.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/d7e9d27c-5c50-4ebb-8788-4bfc584753ff.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/d7e9d27c-5c50-4ebb-8788-4bfc584753ff.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/6e73f7d0-6fb1-466b-9525-5b6b3974194c.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/6e73f7d0-6fb1-466b-9525-5b6b3974194c.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/b00805a6-e88c-4474-b40d-895706446ee9.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/b00805a6-e88c-4474-b40d-895706446ee9.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/169a4d43-d5a7-40cb-9309-754e72823af4.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/169a4d43-d5a7-40cb-9309-754e72823af4.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/073759a5-fc23-4dff-952b-b1bf6c4f3647.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/073759a5-fc23-4dff-952b-b1bf6c4f3647.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/4c903b15-f0ee-4e38-a05d-93d7f8087cae.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/4c903b15-f0ee-4e38-a05d-93d7f8087cae.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/c4eb625d-f3b3-4fbc-a44c-b20cc41f636b.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/c4eb625d-f3b3-4fbc-a44c-b20cc41f636b.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/209b0111-ffb9-44ed-a880-49ba3834cb27.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/209b0111-ffb9-44ed-a880-49ba3834cb27.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/0328b5d8-45ac-4213-bb65-f06dd6593d7b.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/0328b5d8-45ac-4213-bb65-f06dd6593d7b.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/6bb4e009-ea7c-4e97-b55f-22c46b1c5753.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/6bb4e009-ea7c-4e97-b55f-22c46b1c5753.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/b79f943e-1fd4-40ef-b776-72fc651957fc.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/b79f943e-1fd4-40ef-b776-72fc651957fc.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/19ac5541-a1ae-4db2-b5dc-3618549e760e.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/19ac5541-a1ae-4db2-b5dc-3618549e760e.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/a1e5b448-d45b-4f53-8864-24872d8459aa.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/a1e5b448-d45b-4f53-8864-24872d8459aa.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/5a03a778-a4e6-4b20-a990-c3d7d094a5f0.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/5a03a778-a4e6-4b20-a990-c3d7d094a5f0.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/713af3a1-b10b-42ed-9a78-17f330c4189a.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/713af3a1-b10b-42ed-9a78-17f330c4189a.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/567c65dd-3c00-4667-b251-1f6315159f44.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/567c65dd-3c00-4667-b251-1f6315159f44.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/3ed6e451-6b9f-49a2-9b54-dabf89d16d15.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/3ed6e451-6b9f-49a2-9b54-dabf89d16d15.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/c481647a-1476-4a49-a9fe-34dbc30149ee.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/c481647a-1476-4a49-a9fe-34dbc30149ee.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/e1eccf93-1404-4798-bc9c-ba47c223621f.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/e1eccf93-1404-4798-bc9c-ba47c223621f.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/34d0dd67-4b84-420a-90c2-438a01d3fec9.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/34d0dd67-4b84-420a-90c2-438a01d3fec9.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/1a5230ac-5d38-4ef8-bc75-02ae574c906c.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/1a5230ac-5d38-4ef8-bc75-02ae574c906c.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/e9a7ea3c-d256-451d-b619-e30d69d2dc3b.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/e9a7ea3c-d256-451d-b619-e30d69d2dc3b.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/d15820b3-3988-4dd6-835b-24f2034f48d8.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/d15820b3-3988-4dd6-835b-24f2034f48d8.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/53c7fb61-173d-4503-86c5-c0b8b12b1a4a.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/53c7fb61-173d-4503-86c5-c0b8b12b1a4a.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/06b9f245-948b-4b27-affe-ccf883497c58.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/06b9f245-948b-4b27-affe-ccf883497c58.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/631b1b6f-b83f-4f5b-b831-363883c3ff08.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/631b1b6f-b83f-4f5b-b831-363883c3ff08.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/d70490d3-43fe-474e-a452-cd3ca4b4806f.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/d70490d3-43fe-474e-a452-cd3ca4b4806f.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/7ec7045b-6724-46fd-8d27-0cb283cdfe63.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/7ec7045b-6724-46fd-8d27-0cb283cdfe63.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/c8908f6c-8ee9-4f84-9b66-dce995649b3a.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/c8908f6c-8ee9-4f84-9b66-dce995649b3a.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/1921c33c-0434-4a40-bb46-53ee5bd28168.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/1921c33c-0434-4a40-bb46-53ee5bd28168.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/a82a0839-7cdd-49e0-90a2-fd7ce377d980.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/a82a0839-7cdd-49e0-90a2-fd7ce377d980.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/471820c7-6330-41c6-a527-aa3913044db9.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/471820c7-6330-41c6-a527-aa3913044db9.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/676950ad-7bf0-45ef-8f07-4ea4bf424e8b.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/676950ad-7bf0-45ef-8f07-4ea4bf424e8b.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/548efdbd-ce86-4fd7-b162-4b01b329a564.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/548efdbd-ce86-4fd7-b162-4b01b329a564.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/f95ac63b-3249-4a5f-ad96-49537a537062.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/f95ac63b-3249-4a5f-ad96-49537a537062.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/6324f888-8e5d-4e2b-b5c0-d3d6d8d07288.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/6324f888-8e5d-4e2b-b5c0-d3d6d8d07288.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/18e538b1-c74c-4281-9a4e-92014433bfd8.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/18e538b1-c74c-4281-9a4e-92014433bfd8.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/fb612be2-8b9d-4cd9-88fe-e9a1a16900ff.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/fb612be2-8b9d-4cd9-88fe-e9a1a16900ff.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/c3e5a5f2-eabe-49ba-a5b7-ce5ef9d5cc54.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/c3e5a5f2-eabe-49ba-a5b7-ce5ef9d5cc54.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/210abfcb-4377-4b26-9fe9-8426556b0714.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/210abfcb-4377-4b26-9fe9-8426556b0714.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/7b7b3350-98ed-4791-bf83-756b977998ba.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/7b7b3350-98ed-4791-bf83-756b977998ba.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/2bceebfe-c2c7-4d11-8c2c-1e17155f3bf4.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/2bceebfe-c2c7-4d11-8c2c-1e17155f3bf4.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/918be2a3-53b9-41ea-bfe7-c498fee178d2.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/918be2a3-53b9-41ea-bfe7-c498fee178d2.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/480f4821-1db0-429d-a752-ca8ece8ab599.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/480f4821-1db0-429d-a752-ca8ece8ab599.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/97b5edc2-d93e-4547-8dc1-d766d10d684b.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/97b5edc2-d93e-4547-8dc1-d766d10d684b.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/266b0d7b-fdb5-434b-9dd3-c91e65b83fb4.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/266b0d7b-fdb5-434b-9dd3-c91e65b83fb4.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/41ccd3f7-babc-4dd1-b1c8-f01c88fb1868.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/41ccd3f7-babc-4dd1-b1c8-f01c88fb1868.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/ca2c3b9f-9850-4d98-83af-81b092b78385.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/ca2c3b9f-9850-4d98-83af-81b092b78385.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/2652b819-6cd4-46b1-b423-3c0e222017ce.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/2652b819-6cd4-46b1-b423-3c0e222017ce.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/7b930ec5-1a1b-4903-80b9-878b6c05a240.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/7b930ec5-1a1b-4903-80b9-878b6c05a240.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/d5e90675-3da3-49c1-b9af-640b470601c4.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/d5e90675-3da3-49c1-b9af-640b470601c4.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/ad3994e6-5d82-4c1b-8e49-f634dcf13e9a.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/ad3994e6-5d82-4c1b-8e49-f634dcf13e9a.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/636113db-564e-452f-b3b0-f871d863a61e.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/636113db-564e-452f-b3b0-f871d863a61e.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/33e775a6-26e9-4410-831a-551acab4af63.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/33e775a6-26e9-4410-831a-551acab4af63.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/9fc4c270-d359-41d4-97f6-a9c427dfda27.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/9fc4c270-d359-41d4-97f6-a9c427dfda27.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/e4ad21f5-0b36-44ff-9931-bf743607b6df.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/e4ad21f5-0b36-44ff-9931-bf743607b6df.jpg?size=150'
+        },
+        {
+            img: 'https: //images.openlane.eu/carimgs/4247144/general/8901679f-a4c3-4af3-b4e7-24c546967c14.jpg',
+            thumbnail: 'https: //images.openlane.eu/thumbnail/4247144/8901679f-a4c3-4af3-b4e7-24c546967c14.jpg?size=150'
+        }
+    ]
+}
--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,86 @@
-import express, { Application, Request, Response } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
-import fs from 'fs';
-import path from 'path';
-const app: Application = express();
+import { Builder, By, WebDriver, until } from 'selenium-webdriver';
 
-const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'My Microservice API',
-            version: '1.0.0',
-            description: 'API documentation',
-        },
-    },
-    apis: ['./src/routes/*.ts'],
-};
+async function scrapeWithSeleniumGrid() {
+  let driver: WebDriver | null = null;
+  const user = process.env.OPENLANEUSER;
+  const pass = process.env.OPENLANEPASS;
+  if (user === undefined || pass === undefined) {
+    console.error('Please set OPENLANEUSER and OPENLANEPASS environment variables');
+    return;
 
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
+  }
+  try {
+    // Create a WebDriver instance connected to Selenium Grid
+    driver = await new Builder()
+      .usingServer('http://localhost:4444/wd/hub') // Replace with the Selenium Grid hub URL
+      .forBrowser('firefox') // Specify the browser (chrome, firefox, etc.)
+      .build();
+    await driver.get('https://www.openlane.eu/en/home')
+    await driver.sleep(5000);
+    await driver.findElement(By.id('onetrust-accept-btn-handler')).click();
+    await driver.findElement(By.xpath('//*[@id="loginButton2"]')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.xpath('//*[@id="react-root"]/div/div[2]/div[2]/div/main/div/div[1]/div/div/div/input')).sendKeys(user);
+    await driver.sleep(1000);
+    await driver.findElement(By.xpath('//*[@id="loginButton4"]')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.xpath('//*[@id="react-root"]/div/div[2]/div[2]/div/main/div/div[2]/div/div/div/input')).sendKeys(pass);
+    await driver.sleep(1000);
+    await driver.findElement(By.xpath('//*[@id="loginButton3"]')).click();
+    await driver.sleep(1000);
+    await driver.get('https://www.openlane.eu/sl/findcar?fuelTypes=Benzine%2CElectric%2CHybride%2CDiesel&damageTypes=1&auctionTypes=5%2C4%2C2%2C1'); // Replace with your target website
+    await driver.sleep(5000);
 
-app.use(express.json());
+    // Example: Extract elements (e.g., h1 texts)
+    const headings = await driver.findElements(By.xpath('//*[@id="react-root"]/div/div/div/div/div/div/div/section/section/div/div/div/h3'));
+    console.log(headings);
+    var ads: any[] = [];
+    for (const heading of headings) {
+        ads.push({
+            title: await heading.getText(),
+            url: await heading.findElement(By.xpath('a')).getAttribute('href')
+        });
+    }
+    for (var ad of ads) {
+    await driver.get(ad.url);
+    await driver.sleep(5000);
+    const imgElements = await driver.findElements(By.css('.image-gallery-thumbnail-inner img'));
 
-const loadRoutes = (dir: string) => {
-    fs.readdirSync(dir).forEach((file) => {
-        const filePath = path.join(dir, file);
-        const stat = fs.statSync(filePath);
+    ad.firstReg = await driver.findElement(By.xpath("//div[@data-attr='car-first-registration']")).getText();
+    ad.mileage = await driver.findElement(By.xpath("//div[@data-attr='car-mileage']")).getText();
+    ad.fuel = await driver.findElement(By.xpath("//div[@data-attr='car-fuel']")).getText();
+    ad.transmission = await driver.findElement(By.xpath("//div[@data-attr='car-transmission']")).getText();
+    ad.kw = await driver.findElement(By.xpath("//div[@data-attr='car-kw']")).getText();
+    ad.engineSize = await driver.findElement(By.xpath("//div[@data-attr='car-engine']")).getText();
+    ad.vin = await driver.findElement(By.xpath("//div[@data-attr='car-chassisnumber']")).getText();
+    ad.color = await driver.findElement(By.xpath("//div[@data-attr='car-paint']")).getText();
+    ad.ddv = await driver.findElement(By.xpath("//*[@id='react-root']/div/div/div[4]/div[2]/div[1]/div[4]/section[2]/div[2]/div/div/div[2]")).getText();
+    ad.lokacija = await driver.findElement(By.xpath("//a[contains(@class, 'uitest-address-link')]")).getText();
+    ad.possiblePrice = await driver.findElement(By.className('price-amount')).getText();
+    await driver.findElement(By.className('uitest-tab-delivery')).click();
+    await driver.sleep(5000);
+    ad.deliveryPrice = await driver.findElement(By.xpath("//div[contains(@class, 'rc-CarDeliveryOptions')]/div[2]/div/div/div/table/tbody/tr[2]/td[1]/h4")).getText();
+    ad.deliveryTime = await driver.findElement(By.xpath("//div[contains(@class, 'rc-CarDeliveryOptions')]/div[2]/div/div/div/table/tbody/tr[2]/td[1]/p[2]")).getText();
+    ad.imageUrls = await Promise.all(imgElements.map(async (img) => {
+      const parentOfParent = await img.findElement(By.xpath('ancestor::a')).getAttribute('class');
+      const src = (await img.getAttribute('src'));
+      const splitSrc = src.replace('?size=150', '').split('/');
+      return {
+        img: `https://images.openlane.eu/carimgs/${splitSrc[splitSrc.length - 2]}/${parentOfParent.includes('thumbnail-damaged') ? 'damage' : 'general'}/${splitSrc[splitSrc.length - 1]}`,
+        thumbnail: src
+      }
+    }));
+    console.log(ad);
+    }
 
-        if (stat.isDirectory()) {
-            loadRoutes(filePath);
-        } else if (file.endsWith('.ts') || file.endsWith('.js')) {
-            const route = require(filePath).default;
-            app.use('/api', route);
-        }
-    });
-};
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    // Quit the driver
+    if (driver) {
+      await driver.quit();
+    }
+  }
+}
 
-loadRoutes(path.join(__dirname, 'routes'));
-
-
-loadRoutes(path.join(__dirname, 'routes'));
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+scrapeWithSeleniumGrid();
